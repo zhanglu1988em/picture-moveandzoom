47809c8377f3173618f2a80ddaa46b7b6df83afe
zhanglu1988em <zhanglu1988em163.com@zhanglu1988emdemacbook-pro.local>
2016-01-01 20:36:17 +0800

    Initial Commit

---
+++ 01-图片移动/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01-图片移动
+//
+//  Created by zhanglu1988em on 16/1/1.
+//  Copyright © 2016年 wbder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01-图片移动/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01-图片移动
+//
+//  Created by zhanglu1988em on 16/1/1.
+//  Copyright © 2016年 wbder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
